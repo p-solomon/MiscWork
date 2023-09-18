1a5fa88d673546a89079c7b1376bd358944ce360
Peter Solomon <petersolomon002@gmail.com>
2023-09-18 11:32:24 +0100

    Product Added

---
+++ SoftwarePatterns/PatternChoices/app/service/ProductService.java
@@ -0,0 +1,78 @@
+package service;
+
+import Command.AddProductCommand;
+import Factory.ProductFactory;
+import Iterator.AvailProductIterator;
+import com.google.inject.Inject;
+import models.Product;
+import play.data.FormFactory;
+import play.mvc.Http;
+import repository.ProductRepos;
+import repository.UserRepos;
+
+import java.util.List;
+
+public class ProductService {
+
+    private final ProductRepos productRepos;
+    private final UserRepos userRepos;
+    private final FormFactory formFactory;
+
+
+
+    @Inject
+    public ProductService(FormFactory formFactory, ProductRepos productRepos, UserRepos userRepos) {
+        this.productRepos= productRepos;
+        this.formFactory = formFactory;
+        this.userRepos = userRepos;
+    }
+
+    //Added Command Pattern to addProduct
+        public Product addProduct(Http.Request productRequest) throws Exception {
+        AddProductCommand addProductCommand = new AddProductCommand(productRequest, productRepos, userRepos, formFactory);
+
+        return addProductCommand.execute();
+    }
+
+    public Product updateStock(Http.Request productRequest) {
+        Product productObject = formFactory.form(Product.class).bindFromRequest(productRequest).get();
+
+        Product existingProduct = productRepos.getProduct(productObject);
+
+        existingProduct.setStockLevel(existingProduct.getStockLevel() + productObject.getStockLevel());
+        return productRepos.updateProduct(existingProduct);
+    }
+
+    public Product deleteProduct(Http.Request productRequest){
+        Product productObject = formFactory.form(Product.class).bindFromRequest(productRequest).get();
+
+        return productRepos.deleteProduct(productObject);
+    }
+
+    public List<Product> viewStocks(Http.Request productRequest) {
+        Product productObject = formFactory.form(Product.class).bindFromRequest(productRequest).get();
+
+        return productRepos.allAdminStocks(productObject.getId());
+    }
+
+    //ADDED STRATEGY PATTERN, FACTORY PATTERN
+    public Product searchProduct(Http.Request productRequest) {
+        ProductFactory productFactory = new ProductFactory(userRepos, formFactory);
+        Product productObject = productFactory.createProduct(productRequest);
+
+        return productRepos.getProductByName(productObject.getName(), productObject.getUser());
+    }
+
+    //Added Iterator Pattern
+    public List <Product> allProducts(Http.Request productRequest){
+        AvailProductIterator productIterator = new AvailProductIterator(productRepos.allProducts());
+
+        return productIterator.getActiveProducts();
+    }
+
+    public Product getProduct(Product product){
+
+        return productRepos.getProduct(product);
+    }
+
+}
