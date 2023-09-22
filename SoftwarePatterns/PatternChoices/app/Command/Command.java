a8d2e13f936886796f00791cbc4520581e618f8c
Peter Solomon <petersolomon002@gmail.com>
2023-09-22 11:39:26 +0100

    Factory pattern

--- SoftwarePatterns/PatternChoices/app/Command/Command.java
+++ SoftwarePatterns/PatternChoices/app/Command/Command.java
@@ -1,7 +1,5 @@
 package Command;
 
-import models.Product;
-
 public interface Command {
     void execute() throws Exception;
 }
---
+++ SoftwarePatterns/PatternChoices/app/Factory/Factory.java
@@ -0,0 +1,34 @@
+package Factory;
+
+import Strategy.JsonUuid;
+import models.Product;
+import models.User;
+import play.data.FormFactory;
+import play.mvc.Http;
+import repository.UserRepos;
+
+import java.util.UUID;
+
+public class Factory {
+    private final UserRepos userRepos;
+
+    private final FormFactory formFactory;
+    JsonUuid jsonUuid = JsonUuid.getInstance();
+
+
+    public Factory(UserRepos userRepos, FormFactory formFactory) {
+        this.userRepos = userRepos;
+        this.formFactory = formFactory;
+    }
+
+    public Product createProduct(Http.Request productRequest) {
+        Product productObject = formFactory.form(Product.class).bindFromRequest(productRequest).get();
+        UUID uuid = jsonUuid.getUuid(productRequest);
+
+        User existingUser = userRepos.getUser(uuid);
+        productObject.setUser(existingUser);
+
+        return productObject;
+    }
+}
+
