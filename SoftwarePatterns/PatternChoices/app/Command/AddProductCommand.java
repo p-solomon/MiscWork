ecc290ca1f9ab434f65af6da1b543740a926b8ce
Peter Solomon <petersolomon002@gmail.com>
2023-09-22 11:38:39 +0100

    Command pattern

---
+++ SoftwarePatterns/PatternChoices/app/Command/AddProductCommand.java
@@ -0,0 +1,44 @@
+package Command;
+
+import Strategy.JsonUuid;
+import models.Product;
+import models.User;
+import play.data.FormFactory;
+import play.mvc.Http;
+import repository.ProductRepos;
+import repository.UserRepos;
+
+import java.util.UUID;
+
+public class AddProductCommand implements ProductCommand {
+    private final Http.Request productRequest;
+    private final ProductRepos productRepos;
+    private final UserRepos userRepos;
+    private final FormFactory formFactory;
+    JsonUuid jsonUuid = JsonUuid.getInstance();
+
+
+
+    public AddProductCommand(Http.Request productRequest, ProductRepos productRepos, UserRepos userRepos, FormFactory formFactory) {
+        this.productRequest = productRequest;
+        this.productRepos = productRepos;
+        this.userRepos = userRepos;
+        this.formFactory = formFactory;
+    }
+
+    //ADDED STRATEGY PATTERN
+    @Override
+    public Product execute() throws Exception {
+        Product productObject = formFactory.form(Product.class).bindFromRequest(productRequest).get();
+        UUID uuid = jsonUuid.getUuid(productRequest);
+
+        User existingUser = userRepos.getUser(uuid);
+        productObject.setUser(existingUser);
+
+        if(productRepos.getProductByName(productObject.getName(), productObject.getUser()) != null){
+            throw new Exception("Already have a product with this name");
+        }
+
+        return productRepos.insertProduct(productObject);
+    }
+}
